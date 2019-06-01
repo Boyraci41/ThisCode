9faddc673ff07195173f96a821cb9c256de9a2f8
Mücahit Boyracı <40913394+Boyraci41@users.noreply.github.com>
2019-06-02 01:05:26 +0300

    isPrime.py

---
+++ isPrime.py
@@ -0,0 +1,17 @@
+liste=[]
+sayi=input("Lütfen Bir Sayı Giriniz:")
+
+for i in range(1,sayi+1):
+      if sayi%i ==0:
+          liste.append(i)
+          
+          
+            
+            
+              
+if liste==[1,sayi]:
+    print(sayi,"asal sayıdır.")
+    
+    
+else:
+  print(sayi,"asal değildir")
